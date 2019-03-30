219cbe5b9065e85b7061014dbc047dea2825ba68
abdrouf4995 <40562600+abdrouf4995@users.noreply.github.com>
2019-03-30 12:38:23 +0000

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+colnames = ['Pass_id', 'f_id', 'from', 'dest', 'dept_time','tot_time']
+flights = pd.read_csv(r"C:\Users\bc822272\Desktop\AComp_Passenger_data_no_error.csv",names=colnames)
+airports = pd.read_csv(r"C:\Users\bc822272\Desktop\Top30_airports_LatLong.csv")
+
+#part 1
+port1= airports['IATA']
+port2 =flights['from']
+x = []
+y = []
+
+for p1 in port1 :
+    if p1 not in port2.values:
+        x.append(p1)
+    elif p1 in port2.values:
+        y.append(p1)
+print("Used Airports:")
+print(*y, sep = "\n")
+print("\nUnused Airports:")
+print(*x , sep = "\n")
